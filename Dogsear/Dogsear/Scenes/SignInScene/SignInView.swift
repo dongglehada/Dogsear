330406dc9c2310d823d7482c861c502d249e6fe4
= <=>
2023-11-25 01:34:16 +0900

    [FEAT] SignInPage UI 작업

--- Dogsear/Dogsear/Scenes/SignInScene/SignInView.swift
+++ Dogsear/Dogsear/Scenes/SignInScene/SignInView.swift
@@ -20,7 +20,43 @@ final class SignInView: UIView {
     
     let emailTextField = SharedTextField(type: .normal, placeHolder: "이메일")
     let passwordTextField = SharedTextField(type: .password, placeHolder: "패스워드")
+    let autoLoginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("로그인 상태 유지", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.titleLabel?.font = Typography.body3.font
+        button.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
+        button.tintColor = .myPointColor
+        return button
+    }()
+    
     let signInButton = SharedButton(title: "로그인")
+    
+    let buttonsStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.alignment = .trailing
+        view.spacing = Constant.defaults.padding
+        view.distribution = .fill
+        return view
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("회원가입", for: .normal)
+        button.titleLabel?.font = Typography.body3.font
+        button.setTitleColor(.label, for: .normal)
+        return button
+    }()
+    
+    let passwordFindButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("비밀번호 재설정", for: .normal)
+        button.titleLabel?.font = Typography.body3.font
+        button.setTitleColor(.label, for: .normal)
+        return button
+    }()
+    
     
     // MARK: - 생성자
     init() {
@@ -35,6 +71,15 @@ final class SignInView: UIView {
 
 private extension SignInView {
     func setUp() {
+        setUpLogoImageView()
+        setUpEmailTextField()
+        setUpPasswordTextField()
+        setUpAutoLoginButton()
+        setUpSignInButton()
+        setUpButtonStackView()
+    }
+    
+    func setUpLogoImageView() {
         self.addSubview(logoImageView)
         logoImageView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(Constant.screenHeight * 0.2)
@@ -42,19 +87,46 @@ private extension SignInView {
             make.width.equalTo(Constant.screenWidth / 2)
             make.height.equalTo(Constant.screenHeight * 0.05)
         }
+    }
+    
+    func setUpEmailTextField() {
         self.addSubview(emailTextField)
         emailTextField.snp.makeConstraints { make in
-            make.top.equalTo(logoImageView.snp.bottom).offset(Constant.defaults.padding)
+            make.top.equalTo(logoImageView.snp.bottom).offset(Constant.defaults.padding * 2)
             make.left.right.equalToSuperview().inset(Constant.defaults.padding)
         }
+    }
+    
+    func setUpPasswordTextField() {
         self.addSubview(passwordTextField)
         passwordTextField.snp.makeConstraints { make in
             make.top.equalTo(emailTextField.snp.bottom).offset(Constant.defaults.padding)
             make.left.right.equalToSuperview().inset(Constant.defaults.padding)
         }
+    }
+    
+    func setUpAutoLoginButton() {
+        self.addSubview(autoLoginButton)
+        autoLoginButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(Constant.defaults.padding * 2)
+            make.left.equalToSuperview().inset(Constant.defaults.padding)
+        }
+    }
+    
+    func setUpButtonStackView() {
+        self.addSubview(buttonsStackView)
+        buttonsStackView.snp.makeConstraints { make in
+            make.centerY.equalTo(autoLoginButton.snp.centerY)
+            make.right.equalToSuperview().inset(Constant.defaults.padding)
+        }
+        buttonsStackView.addArrangedSubview(passwordFindButton)
+        buttonsStackView.addArrangedSubview(signUpButton)
+    }
+    
+    func setUpSignInButton() {
         self.addSubview(signInButton)
         signInButton.snp.makeConstraints { make in
-            make.top.equalTo(passwordTextField.snp.bottom).offset(Constant.defaults.padding)
+            make.top.equalTo(autoLoginButton.snp.bottom).offset(Constant.defaults.padding)
             make.left.right.equalToSuperview().inset(Constant.defaults.padding)
         }
     }
