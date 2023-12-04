317e0673105f71c9e2efc9fb2768395269b4d6e7
= <=>
2023-12-04 20:36:29 +0900

    [FEAT] creatuser 메서드 구현

--- Dogsear/Dogsear/Managers/FirebaseManager.swift
+++ Dogsear/Dogsear/Managers/FirebaseManager.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import FirebaseStorage
+import Firebase
+import FirebaseFirestoreSwift
+
+class FirebaseManager {
+    
+    private let db = Firestore.firestore()
+    private let users: String = "users"
+    
+    func uploadImage(image: UIImage, pathRoot: String, completion: @escaping (URL?) -> Void) {
+        guard let imageData = image.jpegData(compressionQuality: 0.4) else { return }
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpeg"
+        
+        let imageName = UUID().uuidString + String(Date().timeIntervalSince1970)
+        
+        let firebaseReference = Storage.storage().reference().child("\(imageName)")
+        firebaseReference.putData(imageData, metadata: metaData) { metaData, error in
+            firebaseReference.downloadURL { url, _ in
+                completion(url)
+            }
+        }
+    }
+    
+    func createUser(email: String, nickName: String, completion: @escaping (_ isSuccess: Bool, _ errorMessage: String?) -> Void) {
+        
+        let userData = User(email: email, nickName: nickName, PostBooks: [])
+        do {
+            let data = try Firestore.Encoder().encode(userData)
+            db.collection(users).document(email).setData(data)
+            completion(true, nil)
+        } catch {
+            completion(false,"FirebaseManager_EncodeFail")
+        }
+    }
+    
+}
